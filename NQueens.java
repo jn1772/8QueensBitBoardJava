b56a7ffd8e4eb0ea2f33d5ad59a878fa33fb8358
jn1772 <jn1772@gmail.com>
2014-12-11 00:43:49 +0530

    Print all soved boards.

--- NQueens.java
+++ NQueens.java
@@ -2,6 +2,7 @@ public class NQueens {
     
     private static long[] boards = new long[64];
     private static int solutions = 0;
+    private static boolean[][] displayBoard = new boolean[8][8];
     
     private static void generateBoards(long[] boards){  
         
@@ -51,23 +52,37 @@ public class NQueens {
     } 
     
     public static boolean solve(long board, int position){ 
-        if(position>63) {solutions++;return true;} 
+        if(position>63) {solutions++;printBoard();return true;} 
         
         long saveBoard = board;
+     
         for(int i = 0; i <= 7 ; ++i){
             
             if(!isPlaceable(board, position+i))continue;
+            displayBoard[position/8][i] = true;
             board = board | boards[position+i];
             solve(board, position+8);
             board = saveBoard;
+            displayBoard[position/8][i] = false;
         }  
         return false;
     }
      
+    private static void printBoard(){
+        System.out.println();
+        for(int i=0;i<8;++i){
+            for(int j=0;j<8;++j){
+                if(displayBoard[i][j])System.out.print("Q ");
+                else System.out.print("_ ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
     public static void main(String[] args){
         generateBoards(boards);
         solve(0L, 0);
         System.out.println("No of solutions found = "+solutions);
     }
 }
- 
